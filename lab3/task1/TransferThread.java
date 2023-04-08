61c76411a46f30ad50c6333fba0b1ac762d15987
Maksym <rendon4ik@gmail.com>
2023-04-08 18:06:50 +0300

    Add the first task

---
+++ lab3/task1/TransferThread.java
@@ -0,0 +1,25 @@
+package lab3.task1;
+
+public class TransferThread extends Thread {
+    private Bank bank;
+    private int fromAccount;
+    private int maxAmount;
+    private static final int REPS = 1000;
+
+    public TransferThread(Bank b, int from, int max) {
+        bank = b;
+        fromAccount = from;
+        maxAmount = max;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            for (int i = 0; i < REPS; i++) {
+                int toAccount = (int) (bank.size() * Math.random());
+                int amount = (int) (maxAmount * Math.random() / REPS);
+                bank.transferLock(fromAccount, toAccount, amount);
+            }
+        }
+    }
+}
