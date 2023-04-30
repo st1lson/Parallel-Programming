87b7342f2304ee91e6b8d3a56e5935f6c162e575
Maksym <rendon4ik@gmail.com>
2023-04-30 18:38:29 +0300

    Add non blocking algorithm

--- lab6/src/Program.java
+++ lab6/src/Program.java
@@ -2,13 +2,19 @@ import Models.Matrix;
 
 public class Program {
     public static void main(String[] args) {
-        var firstMatrix = new Matrix(20, 20, 1);
-        var secondMatrix = new Matrix(20, 20, 10);
+        var firstMatrix = new Matrix(2500, 2500, 1);
+        var secondMatrix = new Matrix(2500, 2500, 10);
 
-        var result = new BlockingMultiplier(firstMatrix, secondMatrix, args).multiply();
+        var blockingResult = new BlockingMultiplier(firstMatrix, secondMatrix, args).multiply();
 
-        if (result == null) return;
+        if (blockingResult == null) return;
 
-        System.out.println(result.getMatrix());
+        System.out.println(blockingResult);
+
+//        var result = new NonBlockingMultiplier(firstMatrix, secondMatrix, args).multiply();
+//
+//        if (result == null) return;
+//
+//        System.out.println(result);
     }
 }
