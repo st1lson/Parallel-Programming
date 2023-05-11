0711d6df49f19a352cb8580bcdafb7104e0bfcc2
Maksym <rendon4ik@gmail.com>
2023-05-11 20:12:48 +0300

    Remove test endpoint

--- lab8/src/main/java/com/example/lab8/Controllers/ApiController.java
+++ lab8/src/main/java/com/example/lab8/Controllers/ApiController.java
@@ -28,9 +28,4 @@ public class ApiController {
 
         return ResponseEntity.ok(matrix.getMatrix());
     }
-
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-    }
 }
